ce5837b9a942ff5af126e6b4659fc9ff19cb5240
CHENCHIHIN <2795726586@qq.com>
2025-09-12 09:58:21 +0800

    fomat code

--- text2.cpp
+++ text2.cpp
@@ -2,6 +2,10 @@
 #include<string.h>
 
 char line[1024];
+char key[100][1024]={0};
+char value[100][1024]={0};
+int sum=0;
+
 
 int main(){
 	FILE*a=fopen("data.txt","r");
@@ -14,9 +18,15 @@ int main(){
 			*douhao='\0';
 			char*zhiqian=line;
 			char*zhihou=douhao+1;
-			printf("%s:",zhiqian);
-			printf("%s\n",zhihou);	
+			strcpy(key[sum],zhiqian);
+			strcpy(value[sum],zhihou);
+			sum=sum+1;	
 		}
 	}
-	return 0;
+	for(int i=0;i<=sum-1;i++){
+			printf("第%d个key是%s ",i+1,key[i]);
+			printf("第%d个value是%s\n",i+1,value[i]);
+			printf("%s:",key[i]);
+			printf("%s\n",value[i]);
+	}
 }
